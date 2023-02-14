c35643af3572e3da5ad2db1e39d890f4a33c2400
rafaelefe <rafael_efe@hotmail.com>
2023-02-13 21:56:27 -0300

    [M1S03] Exercício 1

---
+++ modulo_01/semana_03/exercicio_01/exercicio_01/Produto.cs
@@ -0,0 +1,23 @@
+﻿namespace exercicio_01
+{
+    public class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public Produto()
+        {
+            nome = "Sem nome";
+            preco = 0;
+            quantidade = 0;
+        }
+    }
+}
