b828673ef15295bcfbb4bd35506388f2efd5e5a8
rafaelefe <rafael_efe@hotmail.com>
2023-04-11 15:59:40 -0300

    exercicio 2

---
+++ modulo_01/semana_01/exercicio_02/Program.cs
@@ -0,0 +1,30 @@
+﻿// [M1S01] Ex 2 - Organizador de lista de frutas
+// Crie um programa que receba do usuário uma lista de frutas com no máximo 15 itens. Organize as frutas em ordem alfabética e exiba a lista.
+
+List<string> listaFrutas = new List<string>();
+
+bool continuar = true;
+while (continuar)
+{
+    if (listaFrutas.Count() == 15)
+    {
+        continuar = false;
+    }
+    Console.Write("Digite uma fruta: ");
+    listaFrutas.Add(Console.ReadLine());
+    Console.Write("Deseja continuar? Responda [s] para sim ou [n] para não: ");
+
+    string resposta = Console.ReadLine();
+    if (resposta == "n" || resposta == "nao")
+    {
+        continuar = false;
+    }
+}
+
+listaFrutas.Sort();
+
+foreach (string fruta in listaFrutas)
+{
+    Console.WriteLine(fruta);
+}
+
---
+++ modulo_01/semana_03/exercicio_02/Entidades/ProdutoFinanceiro.cs
@@ -0,0 +1,16 @@
+﻿namespace exercicio_02.Entidades
+{
+    internal class ProdutoFinanceiro : Produto
+    {
+        public void acesso()
+        {
+            ProdutoFinanceiro PF1 = new ProdutoFinanceiro();
+            PF1.nome = "Produto dentro da classe ProdutoFinanceiro";
+            PF1.cor = "Verde";
+            Console.WriteLine($"Nome: {PF1.nome} \nCor: {PF1.cor}");
+            PF1.verificarEstoque();
+            PF1.vender();
+            PF1.alterarAtributosPrivados(99, 100.0);
+        }
+    }
+}
