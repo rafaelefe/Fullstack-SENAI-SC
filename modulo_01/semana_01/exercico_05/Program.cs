1e9f80705349e9fd121af3cf42b33580632767be
rafaelefe <rafael_efe@hotmail.com>
2023-04-11 16:23:25 -0300

    exercicio 5

---
+++ modulo_01/semana_01/exercico_05/Program.cs
@@ -0,0 +1,19 @@
+﻿// [M1S01] Ex 5 - Sequência de Fibonacci
+// Faça um algoritmo que receba um número inteiro que será a quantidade de números calculados pela sequência de Fibonacci. Em seguida, exiba a sequência no terminal. 
+
+int n;
+int x = 1, y = 0, z = 0;
+
+string number = Console.ReadLine();
+
+n = int.Parse(number);
+
+for (int i = 0; i < n; i++)
+{
+    z = x + y;
+    Console.WriteLine(z);
+    y = x;
+    x = z;
+}
+Console.ReadLine();
+
