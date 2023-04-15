551cb06d4f6339c47e55f9bfe4b274fdfafd0b36
rafaelefe <rafael_efe@hotmail.com>
2023-04-15 03:23:31 -0300

    exercicio 1

---
+++ modulo_01/semana_05/exercicio_01/Program.cs
@@ -0,0 +1,37 @@
+﻿decimal numero1;
+decimal numero2;
+decimal resultado;
+
+try
+{
+    Console.Write("Digite o primeiro número: ");
+    numero1 = decimal.Parse(Console.ReadLine());
+
+    Console.Write("Digite o segundo número: ");
+    numero2 = decimal.Parse(Console.ReadLine());
+
+    resultado = numero1 / numero2;
+
+    Console.WriteLine($"O resultado da divisão de {numero1} por {numero2} é {resultado}.");
+}
+
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine($"Erro! Não é possível dividir por zero. {ex.Message}.");
+}
+
+catch (FormatException ex)
+{
+    Console.WriteLine($"Erro! Não é possível digitar letras. {ex.Message}");
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro encontrado: {ex.Message}");
+}
+
+finally
+{
+    numero1 = 0;
+    numero2 = 0;
+}
