f6095c03b6155831367b5cb8739b83de1b616088
rafaelefe <rafael_efe@hotmail.com>
2023-04-15 03:25:04 -0300

    exercicio 2

---
+++ modulo_01/semana_05/exercicio_02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+FileInfo arquivo = null;
+
+try
+{
+    Console.WriteLine("Digite o nome do arquivo para criá-lo: ");
+    string nomeArquivo = Console.ReadLine();
+
+    arquivo = new FileInfo(nomeArquivo);
+
+    using (StreamWriter gravadorTexto = File.AppendText(nomeArquivo))
+    {
+        gravadorTexto.WriteLine("Texto de dentro do arquivo.");
+    }
+
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro: {ex.Message}.");
+}
+
+finally
+{
+    Console.WriteLine("Encerrando programa... \n");
+    arquivo = null;
+}
