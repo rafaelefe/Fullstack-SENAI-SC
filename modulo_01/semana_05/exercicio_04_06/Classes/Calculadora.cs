da1d06a0327748355555404e2971266de82dbe23
rafaelefe <rafael_efe@hotmail.com>
2023-04-15 03:40:15 -0300

    exercicios 4 ao 6

---
+++ modulo_01/semana_05/exercicio_04_06/Classes/Calculadora.cs
@@ -0,0 +1,226 @@
+﻿namespace Exercicios04ao06.Classes
+{
+    // exercicios 4 e 5
+    public static class Calculadora
+    {
+        /// <summary>
+        /// Executa a soma de dois números inteiros
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Executa a soma de dois números do tipo double
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static double Somar(double a, double b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Executa a soma de dois números do tipo float
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static float Somar(float a, float b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Executa a soma de dois números decimais
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static decimal Somar(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Executa a soma de dois números do tipo long
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static long Somar(long a, long b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Executa a subtracao de dois números inteiros
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Executa a subtracao de dois números do tipo double
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static double Subtracao(double a, double b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Executa a subtracao de dois números do tipo float
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static float Subtracao(float a, float b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Executa a subtracao de dois números decimais
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static decimal Subtracao(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Executa a subtracao de dois números do tipo long
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static long Subtracao(long a, long b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Executa a multiplicacao de dois números inteiros
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Executa a multiplicacao de dois números do tipo double
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static double Multiplicacao(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Executa a multiplicacao de dois números do tipo float
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static float Multiplicacao(float a, float b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Executa a multiplicacao de dois números decimais
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static decimal Multiplicacao(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Executa a multiplicacao de dois números do tipo long
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static long Multiplicacao(long a, long b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Executa a divisao de dois números inteiros
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static int Divisao(int a, int b)
+        {
+            return a / b;
+        }
+
+        /// <summary>
+        /// Executa a divisao de dois números do tipo double
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static double Divisao(double a, double b)
+        {
+            return a / b;
+        }
+
+        /// <summary>
+        /// Executa a divisao de dois números do tipo float
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static float Divisao(float a, float b)
+        {
+            return a / b;
+        }
+
+        /// <summary>
+        /// Executa a divisao de dois números decimais
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static decimal Divisao(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        /// <summary>
+        /// Executa a divisao de dois números do tipo long
+        /// </summary>
+        /// <param name="a">Primeiro número</param>
+        /// <param name="b">Segundo número</param>
+        /// <returns></returns>
+        public static long Divisao(long a, long b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ modulo_01/semana_05/exercicio_04_06/Classes/ValidadorNumeros.cs
@@ -0,0 +1,62 @@
+﻿namespace Exercicios04ao06.Classes
+{
+    // exercicio 6
+    public static class ValidadorDeNumeros
+    {
+        /// <summary>
+        /// Verifica se um número é primo
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns></returns>
+        public static bool EhNumeroPrimo(int numero)
+        {
+            int contador = 1;
+
+            for (int n = 1; n < numero; n++)
+            {
+                if (numero % n == 0)
+                {
+                    contador = contador + 1;
+                }
+            }
+
+            if (contador == 2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Retorna se um número é par ou ímpar
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns></returns>
+        public static string ParOuImpar(int numero)
+        {
+            if (numero % 2 == 0)
+            {
+                return "Par";
+            }
+
+            return "Impar";
+        }
+
+        /// <summary>
+        /// Verifica se um número é divisível por outro
+        /// </summary>
+        /// <param name="dividendo"></param>
+        /// <param name="divisor"></param>
+        /// <returns></returns>
+        public static bool EhDivisivelPor(int dividendo, int divisor)
+        {
+            if (dividendo % divisor == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
